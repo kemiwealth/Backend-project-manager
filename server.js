aed986afac8ccaf375fba3ac342edc97376be9f6
kemiwealth <kjcartel0@gmail.com>
2025-12-07 00:43:26 -0800

    fixing CORS login error

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 // ========= Middlewares =================
 app.use(morgan('dev')); // logger
 app.use(express.json()); // body parser
-app.use(cors({origin: process.env.FRONTEND_URL}));
+app.use(cors({origin: [ process.env.FRONTEND_URL, "http://localhost:5173", "https://backend-project-manager-4934.onrender.com"], credentials: true,}));
 
 // ========= Routes ======================
 app.use('/api/users', require('./routes/userRoutes'));
